bbeecdb1bcd7b46d7d925fed7d5cc3bc6ffeebbf
Natadecoco Seijin <>
2019-10-28 07:23:04 +0900

    飛行する敵　ほぼ完成　システム面は修正必要かもしれないのでチェックお願いします

--- Assets/Scripts/Game/Enemy/FlyableEnemy.cs
+++ Assets/Scripts/Game/Enemy/FlyableEnemy.cs
@@ -4,12 +4,64 @@ using UnityEngine;
 
 public class FlyableEnemy : Enemy
 {
+    private float dx;
+    private float dy;
     // Update is called once per frame
     void Update()
     {
-        AddVx((P_transform.position.x - E_transform.position.x) / 7500);
-        AddVy((P_transform.position.y - E_transform.position.y) / 7500);
-        Move();
+        Addframe();
+        dx = E_transform.position.x - P_transform.position.x;
+        dy = E_transform.position.y - P_transform.position.y;
+        switch (GetEnemymode())
+        {
+            case 0:
+                if (Getwaitframe() == Getmodeframe())
+                {
+                    ChangeEnemyMode(1);
+                }
+                Move(0);
+                break;
+            case 1:
+                if (Mathf.Sqrt(dx * dx + dy * dy) < 2.5)
+                {
+                    ChangeEnemyMode(2);
+                }
+                Move(1);
+                break;
+            case 2:
+                Debug.Log("甲でき");
+                Move(2);
+                ChangeEnemyMode(3);
+                Resetframe();
+                break;
+            case 3:
+                if(Getattackframe() == Getmodeframe())
+                {
+                    ChangeEnemyMode(1);
+                }
+                if (Getattackframe() / 3 < Getmodeframe())
+                {
+                    E_anim.SetBool("isAttack", false);
+                    Move(4);
+                }
+                break;
+
+        }
+
+    }
+
+    public void ChangeEnemyMode(int mode)
+    {
+        enemymode = mode;
+        switch (mode)
+        {
+            case 1:
+                E_anim.SetBool("isAttack", false);
+                break;
+            case 2:
+                E_anim.SetBool("isAttack", true);
+                break;
+        }
     }
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -19,4 +71,5 @@ public class FlyableEnemy : Enemy
             Attack();
         }
     }
+
 }
