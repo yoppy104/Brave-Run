35307ffecb6c2ec2c8269af27e7a1e7b42c78c1c
Natadecoco Seijin <>
2019-10-31 08:28:08 +0900

    修業が足りませんでした

--- Assets/Scripts/Game/Enemy/Enemy.cs
+++ Assets/Scripts/Game/Enemy/Enemy.cs
@@ -151,7 +151,7 @@ public class Enemy : MonoBehaviour
         return attackcooltime * 60;
     }
 
-    private void OnTriggerEnter(Collision collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Magic")
         {
