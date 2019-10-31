deff59a06a84b62c333254fc5553a3908f275568
Natadecoco Seijin <>
2019-10-31 22:07:47 +0900

    GameStartボタンを押した際、数秒後にシーンチェンジする機能を実装

--- Assets/Scripts/Title/TitleSceneScript.cs
+++ Assets/Scripts/Title/TitleSceneScript.cs
@@ -5,21 +5,33 @@ using UnityEngine.SceneManagement;
 
 public class TitleSceneScript : MonoBehaviour
 {
+    private bool scenechange;
+    private float timecount;
+
     // Start is called before the first frame update
     void Start()
     {
         SoundManager.Instance.StartBGM(SoundManager.BGMType.FANFALE);
+        scenechange = false;
+        timecount = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (scenechange)
+        {
+            timecount -= Time.deltaTime;
+            if (timecount < 0)
+            {
+                SceneManager.LoadScene(ConstNumbers.SCENE_NAME_GAME);
+            }
+        }
     }
 
     public void OnClickGameStart()
     {
         Debug.Log("test");
-        SceneManager.LoadScene(ConstNumbers.SCENE_NAME_GAME);
+        scenechange = true;
     }
 }
